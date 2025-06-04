de25acc4c056525e18316f21a8a450b384f3ebd2
hojmark <1203136+hojmark@users.noreply.github.com>
2025-06-04 15:11:53 +0200

    add lint command

--- src/Cli/Commands/Preview/LintCommand.cs
+++ src/Cli/Commands/Preview/LintCommand.cs
@@ -1,10 +1,37 @@
 using System.CommandLine;
+using Drift.Cli.Abstractions;
+using Drift.Cli.Commands.Global;
+using Drift.Cli.Output;
+using Drift.Cli.Output.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace Drift.Cli.Commands.Preview;
 
 internal class LintCommand : Command {
-  private LintCommand() : base( "lint", "Validate spec and env files" ) {
-    var strict = new Option<bool>( ["--strict"], "Strict mode" );
-    AddOption( strict );
+  internal LintCommand( ILoggerFactory loggerFactory ) : base( "lint", "Validate network spec" ) {
+    AddOption( GlobalParameters.Options.Verbose );
+
+    AddOption( GlobalParameters.Options.OutputFormatOption );
+
+    AddArgument( GlobalParameters.Arguments.SpecOptional );
+
+    this.SetHandler(
+      CommandHandler,
+      new ConsoleOutputManagerBinder( loggerFactory ),
+      GlobalParameters.Arguments.SpecOptional,
+      GlobalParameters.Options.OutputFormatOption
+    );
+  }
+
+  private static async Task<int> CommandHandler(
+    IOutputManager output,
+    FileInfo? specFile,
+    GlobalParameters.OutputFormat outputFormat
+  ) {
+    output.Log.LogDebug( "Running lint command" );
+    
+    // TODO implement
+
+    return ExitCodes.Success;
   }
 }

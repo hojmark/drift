65312d829676cc3c39d5d0e3a311f74e3b20a75a
hojmark <1203136+hojmark@users.noreply.github.com>
2025-08-01 12:47:45 +0200

    feat: tilde (~) path expansion (#11)
    * feat: tilde (~) path expansion
    
    * rebase fix
    
    * add profiles to launchSettings.json

---
+++ src/Cli/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+// For NSubstitue
+[assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
--- src/Cli/Commands/Common/SpecFileResolver.cs
+++ src/Cli/Commands/Common/SpecFileResolver.cs
@@ -6,12 +6,10 @@ namespace Drift.Cli.Commands.Common;
 internal class SpecFileResolver {
   private readonly string _baseDirectory;
   private readonly IOutputManager _output;
-  private readonly ILogOutput _logger;
 
   internal SpecFileResolver( IOutputManager output, string baseDirectory ) {
     _baseDirectory = baseDirectory ?? throw new ArgumentNullException( nameof(baseDirectory) );
     _output = output;
-    _logger = output.Log;
   }
 
   /// <summary>
@@ -32,7 +30,18 @@ internal class SpecFileResolver {
   /// </list>
   /// </summary>
   internal FileInfo? Resolve( string? name, bool? throwsOnNotFound = false ) {
-    var file = name != null ? ResolveByName( name ) : ResolveByDefaults();
+    // Expand ~
+    var home = Environment.GetFolderPath( Environment.SpecialFolder.UserProfile );
+    if ( name != null && name.StartsWith( "~" + Path.DirectorySeparatorChar ) ) {
+      name = Path.Combine( home, name[1..].TrimStart( Path.DirectorySeparatorChar ) );
+    }
+
+    if ( name != null && name == "~" ) {
+      return ResolveByDefaults( home );
+    }
+
+    
+    var file = name != null ? ResolveByName( _baseDirectory, name ) : ResolveByDefaults( _baseDirectory );
 
     if ( file != null ) {
       return file;
@@ -47,70 +56,67 @@ internal class SpecFileResolver {
     return null;
   }
 
-  private FileInfo? ResolveByName( string name ) {
+  private FileInfo? ResolveByName( string directory, string name ) {
     if ( string.IsNullOrWhiteSpace( name ) ) {
-      _logger.LogError( "Cannot be null or empty." );
+      _output.Log.LogError( "Cannot be null or empty." );
       throw new ArgumentException( "Cannot be null or empty.", nameof(name) );
     }
 
-    // Expand
-    var expandedName = Path.GetFullPath( name );
-
     // Priority 1: Exact filename
-    var exactPath = Path.Combine( _baseDirectory, name );
+    var exactPath = Path.Combine( directory, name );
     if ( File.Exists( exactPath ) ) {
-      _logger.LogDebug( "Resolved using exact filename: {Path}", exactPath );
+      _output.Log.LogDebug( "Resolved using exact filename: {Path}", exactPath );
       _output.Normal.WriteLineVerbose( $"Resolved using exact filename: {exactPath}" );
       return new FileInfo( exactPath );
     }
 
-    _logger.LogTrace( "Exact filename not found: {Path}", exactPath );
+    _output.Log.LogTrace( "Exact filename not found: {Path}", exactPath );
 
     // Priority 2: "{name}.spec.yaml"
-    var ymlPath = Path.Combine( _baseDirectory, name + ".spec.yml" );
-    var yamlPath = Path.Combine( _baseDirectory, name + ".spec.yaml" );
+    var ymlPath = Path.Combine( directory, name + ".spec.yml" );
+    var yamlPath = Path.Combine( directory, name + ".spec.yaml" );
     if ( File.Exists( ymlPath ) ) {
-      _logger.LogDebug( "Resolved using {{name}}.spec.yaml file: {Path}", ymlPath );
-      _output.Normal.WriteLineVerbose( $"Resolved using {{name}}.spec.yaml file: {ymlPath}" );
+      _output.Log.LogDebug( "Resolved using {{name}}.spec.yml file: {Path}", ymlPath );
+      _output.Normal.WriteLineVerbose( $"Resolved using {{name}}.spec.yml file: {ymlPath}" );
       return new FileInfo( ymlPath );
     }
 
     if ( File.Exists( yamlPath ) ) {
-      _logger.LogDebug( "Resolved using {{name}}.spec.yaml file: {Path}", yamlPath );
-      _output.Normal.WriteLineVerbose( $"Resolved using {{name}}.spec.yaml file: {yamlPath}" );
+      _output.Log.LogDebug( "Resolved using {{name}}.spec.yml file: {Path}", yamlPath );
+      _output.Normal.WriteLineVerbose( $"Resolved using {{name}}.spec.yml file: {yamlPath}" );
       return new FileInfo( yamlPath );
     }
 
-    _logger.LogTrace( "{{name}}.spec.yaml not found: {Path}", _baseDirectory + $"{name}.spec.yaml" );
+    _output.Log.LogTrace( "{{name}}.spec.[yaml|yml] not found: {Path}", directory + $"{name}.spec.yaml" );
 
     return null;
   }
 
-  private FileInfo? ResolveByDefaults() {
+  private FileInfo? ResolveByDefaults( string directory ) {
     // TODO consider usefulness of this default?
     // Priority 3: "drift.spec.yaml"
-    var driftSpecPath = Path.Combine( _baseDirectory, "drift.spec.yaml" );
+    var driftSpecPath = Path.Combine( directory, "drift.spec.yaml" );
     if ( File.Exists( driftSpecPath ) ) {
-      _logger.LogInformation( "Resolved using drift.spec.yaml file: {Path}", driftSpecPath );
+      _output.Log.LogInformation( "Resolved using drift.spec.yaml file: {Path}", driftSpecPath );
       _output.Normal.WriteLineVerbose( $"Resolved using drift.spec.yaml file: {driftSpecPath}" );
       return new FileInfo( driftSpecPath );
     }
 
-    _logger.LogTrace( "drift.spec.yaml not found: {Path}", driftSpecPath );
+    _output.Log.LogTrace( "drift.spec.yaml not found: {Path}", driftSpecPath );
 
     // Priority 4: "*.spec.yaml"
     var specFiles = Directory
-      .EnumerateFiles( _baseDirectory, "*.spec.yaml" )
-      .Concat( Directory.EnumerateFiles( _baseDirectory, "*.spec.yml" ) )
+      .EnumerateFiles( directory, "*.spec.yaml" )
+      .Concat( Directory.EnumerateFiles( directory, "*.spec.yml" ) )
       .ToList();
 
     if ( specFiles.Any() ) {
       var asteriskSpecPath = specFiles.First();
-      _logger.LogInformation( "Resolved using *.spec.yaml file: {Path}", asteriskSpecPath );
+      _output.Log.LogInformation( "Resolved using *.spec.yaml file: {Path}", asteriskSpecPath );
       _output.Normal.WriteLineVerbose( $"Resolved using *.spec.yaml file: {asteriskSpecPath}" );
 
       if ( specFiles.Count > 1 ) {
-        _logger.LogWarning(
+        _output.Log.LogWarning(
           "Found multiple spec files in directory: {SpecFiles}",
           string.Join( ", ", specFiles )
         );

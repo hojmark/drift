1a8683d9e5b27ea4e862c7dac5b2c15ec73531aa
hojmark <1203136+hojmark@users.noreply.github.com>
2025-08-31 20:41:53 +0200

    test: establish architecture tests

---
+++ src/ArchTests/NamingTests.cs
@@ -0,0 +1,52 @@
+using ArchUnitNET.Domain;
+using ArchUnitNET.NUnit;
+using Drift.ArchTests.Fixtures;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Drift.ArchTests;
+
+public class NamingTests : DriftArchitectureFixture {
+  [Test]
+  public void InterfacesShouldStartWithI() {
+    var rule = Interfaces()
+      .Should()
+      .HaveNameStartingWith( "I" )
+      .Because( "Interface naming convention should be followed" );
+
+    rule.Check( DriftArchitecture );
+  }
+
+  [Explicit( "Fix architecture" )] //TODO
+  [Test]
+  public void TestClassesShouldEndWithTests() {
+    /*
+     * Assembly names are "FullName" e.g. "Drift.Cli.E2ETests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
+     */
+    /*var testClasses = Classes()
+      .That()
+      .ResideInAssemblyMatching( @".*\.Tests," )
+      .Or()
+      .ResideInAssemblyMatching( @".*\.E2ETests," );*/
+
+    var rule = Members().That()
+      .HaveAnyAttributes( typeof(TestAttribute) )
+      .Should()
+      .BeDeclaredInTypesThat()
+      .HaveNameEndingWith( "Tests" )
+      .Because( "test classes should follow naming convention" );
+
+    rule.Check( DriftArchitecture );
+  }
+
+  private static void PrintTypes( IObjectProvider<IType> types ) {
+    var classes = types.GetObjects( DriftArchitecture ).ToList();
+
+    if ( classes.Count == 0 ) {
+      Console.WriteLine( "[none]" );
+    }
+
+    foreach ( var testClass in classes ) {
+      Console.WriteLine( testClass.FullName );
+    }
+  }
+}

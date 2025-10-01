17a6b293d3eff946808428df859d5a724a3b722c
hojmark <1203136+hojmark@users.noreply.github.com>
2025-10-01 21:53:39 +0200

    cleanup

--- src/Cli/AssemblyInfo.cs
+++ src/Cli/AssemblyInfo.cs
@@ -2,6 +2,3 @@ using System.Runtime.CompilerServices;
 
 // For NSubstitue
 [assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
-
-// For tests
-[assembly: InternalsVisibleTo( "Drift.Cli.Tests" )]
--- src/Spec/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo( "Drift.Spec.Tests" )]

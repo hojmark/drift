aefb4c4387df88397534f17b42b783bd1fd44718
hojmark <1203136+hojmark@users.noreply.github.com>
2025-10-03 23:08:24 +0200

    fixup

--- src/Scanning/Arp/IArpTableProvider.cs
+++ src/Scanning/Arp/IArpTableProvider.cs
@@ -1,10 +1,16 @@
 namespace Drift.Scanning.Arp;
 
 internal interface IArpTableProvider {
+  /// <summary>
+  /// Gets an application cached ARP table.
+  /// </summary>
   internal ArpTable Cached {
     get;
   }
 
+  /// <summary>
+  /// Gets a fresh ARP table from the OS. The OS may be cache it.
+  /// </summary>
   internal ArpTable Fresh {
     get;
   }

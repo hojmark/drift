00d7336516de5d88bf30964a10f976d901048746
hojmark <1203136+hojmark@users.noreply.github.com>
2025-11-20 23:12:37 +0100

    ignore false positive

--- src/Common/EmbeddedResources/Extensions.cs
+++ src/Common/EmbeddedResources/Extensions.cs
@@ -8,7 +8,10 @@ public static class Extensions {
   }
 
   extension( Stream stream ) {
+    // Justification: false positive (https://community.sonarsource.com/t/fp-s2325-does-report-on-c-14-extensions/151936)
+#pragma warning disable S2325
     public string ReadText( Encoding? encoding = null ) {
+#pragma warning restore S2325
       return new StreamReader( stream, encoding ?? Encoding.UTF8 ).ReadToEnd();
     }
   }

5de6cc044a37f128f5e9b014e6081ea6f4f6a21a
hojmark <1203136+hojmark@users.noreply.github.com>
2025-12-02 22:20:19 +0100

    tests and refactor

--- src/Agent.PeerProtocol/Adopt/IAdoptRequestHandler.cs
+++
@@ -1,7 +0,0 @@
-namespace Drift.Agent.PeerProtocol.Adopt;
-
-internal interface IAdoptRequestHandler {
-  public string MessageType => "adopt-request";
-
-  Task HandleAsync( AdoptRequestPayload message, CancellationToken cancellationToken = default );
-}

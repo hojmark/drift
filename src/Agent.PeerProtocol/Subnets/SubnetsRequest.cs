10e194859b8205e3b38cf35477d0ef4aba858cc8
hojmark <1203136+hojmark@users.noreply.github.com>
2025-12-02 22:20:19 +0100

    tests and refactor

--- src/Agent.PeerProtocol/Subnets/SubnetsRequest.cs
+++ src/Agent.PeerProtocol/Subnets/SubnetsRequest.cs
@@ -4,11 +4,7 @@ using Drift.Networking.PeerStreaming.Core.Abstractions;
 
 namespace Drift.Agent.PeerProtocol.Subnets;
 
-public sealed class SubnetsRequest : IPeerMessage {
-  static SubnetsRequest() {
-    PeerProtocolTypesProvider.Map[MessageType] = JsonInfo;
-  }
-
+public sealed class SubnetsRequest : IPeerRequestMessage {
   public static string MessageType => "subnetsrequest";
 
   public static JsonTypeInfo JsonInfo => SubnetsRequestJsonContext.Default.SubnetsRequest;

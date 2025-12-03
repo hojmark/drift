1fa930a42a05a22e7576819f577d0b648bc6ce4c
hojmark <1203136+hojmark@users.noreply.github.com>
2025-12-03 22:31:50 +0100

    subnet source

--- src/Scanning/Subnets/CompositeSubnetProvider.cs
+++ src/Scanning/Subnets/CompositeSubnetProvider.cs
@@ -1,12 +1,10 @@
-using Drift.Domain;
-
 namespace Drift.Scanning.Subnets;
 
 // TODO needed?
 public class CompositeSubnetProvider( IEnumerable<ISubnetProvider> providers ) : ISubnetProvider {
   private readonly List<ISubnetProvider> _providers = providers.ToList();
 
-  public async Task<List<CidrBlock>> GetAsync() {
+  public async Task<List<ResolvedSubnet>> GetAsync() {
     var results = await Task.WhenAll( _providers.Select( p => p.GetAsync() ) );
     return results.SelectMany( x => x ).Distinct().ToList();
   }

4dcd389155017e3438eef8d378ccc866762c0073
hojmark <1203136+hojmark@users.noreply.github.com>
2025-12-10 22:23:04 +0100

    naming

--- src/Spec/Schema/SpecSchemaProvider.cs
+++ src/Spec/Schema/SpecSchemaProvider.cs
@@ -5,14 +5,14 @@ using Json.Schema;
 namespace Drift.Spec.Schema;
 
 public static class SpecSchemaProvider {
-  private static readonly ConcurrentDictionary<SpecVersion, JsonSchema> SchemaCache = new();
+  private static readonly ConcurrentDictionary<SpecVersion, JsonSchema> Cache = new();
 
   internal static string GetAsText( SpecVersion version ) {
     return EmbeddedResourceProvider.GetStream( GetPath( version ) ).ReadText();
   }
 
   internal static JsonSchema Get( SpecVersion version ) {
-    return SchemaCache.GetOrAdd( version, v => JsonSchema.FromText( GetAsText( v ) ) );
+    return Cache.GetOrAdd( version, v => JsonSchema.FromText( GetAsText( v ) ) );
   }
 
   private static string GetPath( SpecVersion version ) {

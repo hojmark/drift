cbf00cf81c22c5ea9688c584211e932ea2e3e0e2
hojmark <1203136+hojmark@users.noreply.github.com>
2026-03-03 22:08:31 +0100

    fix warning

--- src/Cli.Settings.Tests/TemporarySettingsLocationProvider.cs
+++ src/Cli.Settings.Tests/TemporarySettingsLocationProvider.cs
@@ -2,7 +2,10 @@ using Drift.Cli.Settings.Serialization;
 
 namespace Drift.Cli.Settings.Tests;
 
+// Currently used by FeatureFlagTests in Cli.Tests
+#pragma warning disable CA1515
 public sealed class TemporarySettingsLocationProvider : ISettingsLocationProvider {
+#pragma warning restore CA1515
   private readonly string _directory = Path.Combine( Path.GetTempPath(), Guid.NewGuid().ToString() );
 
   public string GetDirectory() => _directory;

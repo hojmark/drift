4dc189bfb64bdf6f6e6576132302685b8b5d6060
hojmark <1203136+hojmark@users.noreply.github.com>
2026-07-13 21:40:35 +0200

    f

--- src/Agent.Host/AgentHostAssemblyMarker.cs
+++
@@ -1,6 +0,0 @@
-namespace Drift.Agent.Host;
-
-// Justification: marker class
-#pragma warning disable S2094
-public sealed class AgentHostAssemblyMarker;
-#pragma warning restore S2094
---
+++ src/Coordinator.Host/AgentHost.cs
@@ -0,0 +1,12 @@
+using Drift.Messaging.Protocol;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Drift.Coordinator.Host;
+
+public static class CoordinatorHost {
+  // TODO implement
+}

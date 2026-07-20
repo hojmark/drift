0476f173a55ede3961d42b155f139911b50ecdea
hojmark <1203136+hojmark@users.noreply.github.com>
2026-07-20 22:56:46 +0200

    f

--- src/Scanning/Arp/LinuxArpTableProvider.cs
+++ src/Scanning/Arp/LinuxArpTableProvider.cs
@@ -36,7 +36,7 @@ internal class LinuxArpTableProvider : ArpTableProviderBase {
 
       if (
         parts[0].Count( c => c == '.' ) != 3 && // Dots in an IPv4 address
-        parts[1].Count( c => c == ':' ) != 5 // Hyphens in a Linux-reported MAC. E.g., 00:11:22:33:44:55
+        parts[1].Count( c => c == ':' ) != 5 // Semicolons in a Linux-reported MAC. E.g., 00:11:22:33:44:55
       ) {
         Console.Error.WriteLine( $"Skipping invalid ARP entry: {line}" );
         continue;
